0b03b322863bc89ade79fcd56f2c3385f75b5824
Andrii Zhabchyk <andrii.zhabchyk@gmail.com>
2017-08-10 22:42:24 +0300

    Add project

---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+let gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssmin = require('gulp-clean-css'),
+    jsmin = require('gulp-uglify'),
+    webserver = require('gulp-webserver'),
+    livereload = require('gulp-livereload');
+
+let path = {
+    html: [
+        './*.html'
+    ],
+    styles: {
+        src: [
+            './node_modules/bootstrap/dist/css/bootstrap.css',
+            './assets/styles/index.less'
+        ],
+        dependencies: [
+            './assets/styles/'
+        ],
+        name: 'style',
+        dest: './build/'
+    },
+    scripts: {
+        src: [
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './assets/js/*.js'
+        ],
+        name: 'scripts',
+        dest: './build/'
+    }
+};
+let watchersPath = {
+    html: [
+        './*.html'
+    ],
+    styles: [
+        './assets/styles/*.less'
+    ],
+    scripts: [
+        './assets/js/*.js'
+    ]
+};
+
+gulp.task('html', function () {
+    return gulp.src(path.html)
+        .pipe(livereload());// notify livereload server about changes
+});
+
+gulp.task('style', function () {
+    return gulp.src(path.styles.src)
+        .pipe(concat('style.less'))
+        .pipe(less({
+            paths: path.styles.dependencies
+        }))
+        /*.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))*/
+        .pipe(rename(path.styles.name + '.css'))
+        .pipe(gulp.dest(path.styles.dest))
+        .pipe(cssmin())
+        .pipe(rename(path.styles.name + '.min.css'))
+        .pipe(gulp.dest(path.styles.dest))
+        .pipe(livereload());
+});
+
+gulp.task('scripts', function () {
+    return gulp.src(path.scripts.src)
+        .pipe(concat('scripts.js'))
+        .pipe(rename(path.scripts.name + '.js'))
+        .pipe(gulp.dest(path.scripts.dest))
+        /*.pipe(jsmin())*/
+        .pipe(rename(path.scripts.name + '.min.js'))
+        .pipe(gulp.dest(path.scripts.dest))
+        .pipe(livereload());
+});
+
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            open: true,
+            fallback: 'index.html'
+        }));
+});
+
+gulp.task('watch', ['style', 'scripts', 'webserver'], function() {
+    livereload.listen();
+    gulp.watch(watchersPath.html, ['html']);
+    gulp.watch(watchersPath.styles, ['style']);
+    gulp.watch(watchersPath.scripts, ['scripts']);
+});
+
+gulp.task('build', ['style', 'scripts']);
+
+gulp.task('default', ['build']);
+
+
