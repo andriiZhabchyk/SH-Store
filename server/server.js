45d092ee65f1544b35852e9007f933b47e5e7492
andriiZhabhyk <andrii.zhabchyk@gmail.com>
2017-09-01 17:59:45 +0300

    Add filters to subcategory & item single template

--- server/server.js
+++ server/server.js
@@ -63,8 +63,66 @@ app.get('/:category/:subcategory/filters', (req, res) => {
     });
 });
 
+/*app.get('/:category/:subcategory/price', (req, res) => {
+    const category = req.params.category,
+        subcategories = req.params.subcategory;
+
+    dBase.collection(category).findOne({subcategory: subcategories}, {$min: '$items.price'}, (err, price) => {
+        res.render('Filters.hbs', subcategory);
+         res.send(price);
+    });
+});*/
+
+app.get('/:category/:subcategory/:id', (req, res) => {
+    let itemData = {
+        category: req.params.category,
+        subcategory: req.params.subcategory
+    };
+
+    dBase.collection(req.params.category).findOne({subcategory: req.params.subcategory}, (err, items) => {
+        if (err) {
+            res.send({'error': 'An error has occurred'});
+        } else {
+            for (let item of items.items) {
+                if (item.id == req.params.id) {
+                    itemData.item = item;
+                    res.render('single.hbs', itemData);
+                    break;
+                    /*res.send(itemData);*/
+                }
+            }
+        }
+    });
+});
+
+
+
+app.post('/women/coats/search?', (req, res) => {
+    const minPrice = req.body.minPrice,
+        maxPrice = req.body.maxPrice,
+        country = req.body.country,
+        brand = req.body.brand,
+        size = req.body.size;
+
+    const item = {
+        minPrice: minPrice,
+        maxPrice: maxPrice,
+        country: country,
+        brand: brand,
+        size: size
+    };
+
+    res.send(item);
+});
+
+
+
 app.all('/', function root(req, res) {
     res.sendFile(path.resolve('public', 'index.html'));
+});
+
+app.get('/login', function root(req, res) {
+    res.sendFile(path.resolve('public/assets/pages', 'login.html'));
 });
 
 MongoClient.connect(db.url, (err, database) => {
